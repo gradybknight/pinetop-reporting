c81ab9d76fc66499731e3823eea2d02b89f80aec
gradybknight <grady.knight@gmail.com>
2018-08-05 17:11:48 -0400

    phidget working properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,8 @@ const mustBeLoggedIn = require('../shared/middleware/mustBeLoggedIn');
 const phidget22 = require('phidget22');
 
 let serverPotStatus = false;
-let isPotAllowedToRun = true;
+let serverGraphData = [];
+
 
 
 function getCurrentUser(req, res) {
@@ -139,23 +140,45 @@ const flashingLights = {
           if (lcdDisplay.getDeviceID() === phidget22.DeviceID.PN_1204)
             lcdDisplay.setScreenSize(phidget22.LCDScreenSize.DIMENSIONS_2X40);
           lcdDisplay.setBacklight(1);
-          lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 0, "Two Lines!");
-          lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 1, "Phidgets");
+          lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 0, "LED Status: True");
+          lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 1, "Temperature: ");
           lcdDisplay.flush();
           
-          var exTimer;
-
 	        function updateState() {
             var newState = !digitalOutput.getState();
             console.log('\nSetting state to ' + newState + ' for 5 seconds...');
             digitalOutput.setState(newState);
-            let message = `output is ${!newState}`;
+            let message = `LED status is ${!newState}`;
+            let tempMessage = `Temperature is XX F`;
             lcdDisplay.clear();
             lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 0, message);
+            lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 1, tempMessage);
             lcdDisplay.flush();
+
+            let graphDataPoint = {
+              x: Date(),
+              y: Math.random()*100,
+              id: parseInt(Math.random()*10000)
+            }
+            serverGraphData.push(graphDataPoint);
+          
           }
           
-          exTimer = setInterval(function () { updateState() }, 5000);
+          let exTimer = setInterval(function () { 
+            if (serverPotStatus) {
+              updateState() 
+            } else {
+              digitalOutput.setState(true);
+              lcdDisplay.clear();
+              lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 0, 'turning off');
+              lcdDisplay.writeText(phidget22.LCDFont.DIMENSIONS_5X8, 0, 1, 'good bye');
+              lcdDisplay.flush();
+              clearInterval(exTimer);
+              lcdDisplay.close();
+              digitalOutput.close();
+              console.log(serverGraphData);
+            }
+          }, 3000);
         })
         .catch(function (err) {
           console.error('Error running example:', err.message);
